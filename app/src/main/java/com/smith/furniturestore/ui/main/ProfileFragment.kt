30242521b53ef7dcb19bdff42b2cc923004d26e7
smithjilks <smithjilks@gmail.com>
2022-04-06 21:51:27 +0300

    Create main fragments UI layouts

--- app/src/main/java/com/smith/furniturestore/ui/main/ProfileFragment.kt
+++ app/src/main/java/com/smith/furniturestore/ui/main/ProfileFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.smith.furniturestore.databinding.FragmentOrdersBinding
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.smith.furniturestore.R
+import com.smith.furniturestore.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
-    private var _binding: FragmentOrdersBinding? = null
+    private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -16,12 +19,26 @@ class ProfileFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentOrdersBinding.inflate(inflater, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.userProfileCreateItemButton.setOnClickListener {
+            findNavController().navigate(R.id.action_profileFragment_to_createItemFragment)
+
+        }
+
+        binding.userProfileOrdersButton.setOnClickListener {
+            findNavController().navigate(R.id.action_profile_fragment_to_orders_fragment)
+
+        }
+
+        binding.userProfileCartButton.setOnClickListener {
+            findNavController().navigate(R.id.action_profile_fragment_to_cart_fragment)
+
+        }
 
     }
 
