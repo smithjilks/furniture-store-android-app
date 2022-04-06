dcd736e31cff347c631038767f3b0b5a0ec031cb
smithjilks <smithjilks@gmail.com>
2022-04-06 19:51:22 +0300

    Create auth fragments layouts

--- app/src/main/java/com/smith/furniturestore/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/smith/furniturestore/ui/auth/LoginFragment.kt
@@ -1,12 +1,15 @@
 package com.smith.furniturestore.ui.auth
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.smith.furniturestore.R
 import com.smith.furniturestore.databinding.FragmentLoginBinding
+import com.smith.furniturestore.ui.MainActivity
 
 class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
@@ -24,6 +27,20 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.getAccountButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
+        binding.forgotPasswordButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_forgotPasswordFragment)
+        }
+
+        binding.loginButton.setOnClickListener {
+            val intent = Intent(context, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+
+        }
 
     }
 
--- app/src/main/java/com/smith/furniturestore/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/smith/furniturestore/ui/auth/RegisterFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.smith.furniturestore.R
 import com.smith.furniturestore.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
@@ -22,6 +24,10 @@ class RegisterFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.signupHaveAccountButton.setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+        }
 
 
     }
